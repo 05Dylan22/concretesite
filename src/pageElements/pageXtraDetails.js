d8b2e5c141e01eca278fbe29fa795e8eb2baebb6
Dylan Anderson <anderson.dylan.522@gmail.com>
2024-01-24 09:39:45 -0500

    key issue fixed

--- src/pageElements/pageXtraDetails.js
+++ src/pageElements/pageXtraDetails.js
@@ -1,6 +1,6 @@
 import "../styles/pageXtraDetails.css"
 
-const PageXtraDetails = ({lineTitle, info, info2}) => {
+const PageXtraDetails = ({lineTitle}) => {
   return (
     <div className="pg-xtra-details">
         <div className="pg-xtra-details-title-div">
@@ -8,8 +8,12 @@ const PageXtraDetails = ({lineTitle, info, info2}) => {
           <p>{lineTitle}</p>
         </div>
         <div className="pg-xtra-details-info">
-          <p>{info}</p>
-          <p>{info2}</p>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+          </p>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
         </div>
       </div>
   )
--- src/pages/allServicesPage.js
+++ src/pages/allServicesPage.js
@@ -13,7 +13,7 @@ const AllServicesPage = () => {
   return (
     <>
       <PageHero title={"All Services"} img={"https://th.bing.com/th/id/OIP.kg5hzWb8m7ISPbJF3wWxOgHaFj?pid=ImgDet&rs=1"}/>
-      <PageXtraDetails lineTitle={"Who We Are"} info={["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", <br/>, <br/>, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]}/>
+      <PageXtraDetails lineTitle={"Who We Are"}/>
       <HomeServicesSect title={"How Can We Help You"}/>
     </>
   )
